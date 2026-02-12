71bc7aa5830f3bacb23e9f8844e9db88233d2dfb
RtlZeroMemory <58250858+RtlZeroMemory@users.noreply.github.com>
2026-02-12 13:44:26 +0400

    Remove in-repo ink-compat package and mark compatibility as redesign WIP

--- packages/ink-compat/src/context/AccessibilityContext.ts
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const AccessibilityContext = React.createContext<boolean>(false);
-
-export default AccessibilityContext;
--- packages/ink-compat/src/props.ts
+++
@@ -1,4 +0,0 @@
-export { mapBoxProps } from "./props/boxProps.js";
-export type { MappedInkBox } from "./props/boxProps.js";
-export { mapTextProps } from "./props/textProps.js";
-export type { MappedInkText } from "./props/textProps.js";
